44d202d065be0629a426993348dc5b5e56da8873
mr-vishwet <mr.vishwet2705@gmail.com>
2023-12-02 20:39:06 +0530

    Project Added

---
+++ Android/app/src/main/java/io/github/Travel_Mate/login/LoginView.java
@@ -0,0 +1,31 @@
+package TravelMate.login;
+
+/**
+ * Created by el on 5/4/17.
+ */
+
+interface LoginView {
+
+    void rememberUserInfo(String token, String email);
+
+    void startMainActivity();
+
+    void showError();
+
+    void showLoadingDialog();
+
+    void dismissLoadingDialog();
+
+    void getRunTimePermissions();
+
+    void checkUserSession();
+
+    void openSignUp();
+
+    void openLogin();
+
+    void setLoginEmail(String email);
+
+    void showMessage(String message);
+
+}
